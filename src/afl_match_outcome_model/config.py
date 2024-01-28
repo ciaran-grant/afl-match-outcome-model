663710b8bd086a751f05e01b47d91accb71ce1dc
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2024-01-28 20:23:38 +1100

    More cleaning of old code and notebooks

--- src/afl_match_outcome_model/config.py
+++
@@ -1,3 +0,0 @@
-# Raw Data
-match_summary_file_path = "/Users/ciaran/Documents/Projects/AFL/data/match_summary.csv"
-player_stats_file_path = "/Users/ciaran/Documents/Projects/AFL/data/player_stats.csv"
