923fdc2143f10dfc4bec4f7cf3150e6ebf7803b9
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2024-04-12 20:05:14 +1000

    Undoing previous

--- src/afl_match_outcome_model/predict/predict_margin.py
+++ src/afl_match_outcome_model/predict/predict_margin.py
@@ -2,12 +2,12 @@ import joblib
 
 def load_margin_model():
     
-    model_file_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/afl-match-outcome-model/model_outputs/match_margin_xgb_v10.joblib"
+    model_file_path = "model_outputs/match_margin_xgb_v10.joblib"
     
     return joblib.load(model_file_path)
 
 def load_margin_preprocessor():
     
-    preproc_file_path = "/Users/ciaran/Documents/Projects/AFL/git-repositories/afl-match-outcome-model/model_outputs/match_margin_pipeline_v10.joblib"
+    preproc_file_path = "model_outputs/match_margin_pipeline_v10.joblib"
     
     return joblib.load(preproc_file_path)
